82b122cd6ecc058c29d0333ad4bf35e5b5e52761
longA <you@example.com>
2020-02-21 18:58:52 +0700

    Updates

---
+++ precache-manifest.76136c33a58841889a45a918cb1d7fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a4f3f82bf3b4b647a919a3f30859b9",
+    "url": "/rmdb/index.html"
+  },
+  {
+    "revision": "8b4521ff170e8988e545",
+    "url": "/rmdb/static/css/main.68a2efd5.chunk.css"
+  },
+  {
+    "revision": "10b11a193fb4eb392022",
+    "url": "/rmdb/static/js/2.5298f31d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/rmdb/static/js/2.5298f31d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4521ff170e8988e545",
+    "url": "/rmdb/static/js/main.02274a80.chunk.js"
+  },
+  {
+    "revision": "d6260c1baae1d3fa17cf",
+    "url": "/rmdb/static/js/runtime-main.70f67354.js"
+  }
+]);
